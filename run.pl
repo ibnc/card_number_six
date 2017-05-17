dff21365cb73ef2e6a2165399c10015f12bfa82f
Isabelle Carter <isabellenataliecarter@gmail.com>
2017-05-17 13:42:22 -0700

    Adding another comment

--- run.pl
+++ run.pl
@@ -1,5 +1,10 @@
 #!/usr/bin/env swipl
 
+/*
+* The files run.pl and load.pl are "standard" special files
+* see: http://www.swi-prolog.org/pldoc/man?section=project-special-files
+*/
+
 /* this is how you load other predicates from another file */
 :-['load.pl'].
 
